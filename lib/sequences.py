bfa0ad9a17517ecc785c9a7a7e12217952b6c6a3
zeid <zeidhassan4@gmail.com>
2024-03-02 18:49:49 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    a,b = 0,1
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b=b, a+b
+    print(f"[{', '.join(map(str, fibonacci_sequence))}]")
+print_fibonacci(9)    
